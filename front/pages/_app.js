aa26190e1781d0f7b61ef975a7065afd57dc9383
YHJ <yhj@YHJui-MacBookPro.local>
2022-07-14 15:31:51 +0900

    delete about page

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,27 +1,56 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Head from 'next/head';
+// import 'antd/dist/antd.css';
+// // import withReduxSaga from 'next-redux-saga';
+// import wrapper from '../store/configureStore'; 
+
+
+// const NodeBird = ({ Component }) => {
+//     return (
+//         <>
+//         <Head>
+//             <meta charSet="utf-8"/>
+//             <title>NodeBird</title>
+
+//         </Head>
+//         <Component/>
+        
+//         </>
+//     )
+// }
+
+// NodeBird.propTypes = {
+//     Component: PropTypes.elementType.isRequired,
+// };
+
+// export default wrapper.withRedux(NodeBird);
+
+
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
-// import withReduxSaga from 'next-redux-saga';
-import wrapper from '../store/configureStore'; 
 
+import wrapper from '../store/configureStore';
 
-const NodeBird = ({ Component }) => {
-    return (
-        <>
-        <Head>
-            <meta charSet="utf-8"/>
-            <title>NodeBird</title>
-
-        </Head>
-        <Component/>
-        
-        </>
-    )
-}
+const NodeBird = ({ Component }) => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <title>NodeBird</title>
+    </Head>
+    <Component />
+  </>
+);
 
 NodeBird.propTypes = {
-    Component: PropTypes.elementType.isRequired,
+  Component: PropTypes.elementType.isRequired,
 };
+
+// export function reportWebVitals(metric) {
+//   console.log(metric);
+// }
 
 export default wrapper.withRedux(NodeBird);

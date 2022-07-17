4824b54dcd4aeea3a07f9bac1092ffff9eba7caa
YHJ <yhj@YHJui-MacBookPro.local>
2022-07-17 16:44:48 +0900

    prefetch

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -107,7 +107,7 @@ const PostCard = ({ post }) => {
                             <div style={{ float: 'right' }}>{moment(post.createdAt).format('YYYY.MM.DD')}</div>
                             <Card.Meta
                                 avatar={(
-                                    < Link href={`/user/${post.Retweet.User.id}`}>
+                                    < Link href={`/user/${post.Retweet.User.id}`} prefetch={false}>
                                         <a><Avatar>{post.Retweet.User.nickname[0]}</Avatar></a>
                                     </Link>
                                 )}
@@ -124,7 +124,7 @@ const PostCard = ({ post }) => {
                             <div style={{ float: 'right' }}>{moment(post.createdAt).format('YYYY.MM.DD')}</div>
                             <Card.Meta
                                 avatar={(
-                                    < Link href={`/user/${post.User.id}`}>
+                                    < Link href={`/user/${post.User.id}`} prefetch={false}>
                                         <a><Avatar>{post.User.nickname[0]}</Avatar></a>
                                     </Link>
                                 )}
@@ -150,7 +150,7 @@ const PostCard = ({ post }) => {
                                 <Comment
                                     author={item.User.nickname}
                                     avatar={(
-                                        < Link href={`/user/${item.User.id}`}>
+                                        < Link href={`/user/${item.User.id}`} prefetch={false}>
                                             <a><Avatar>{item.User.nickname[0]}</Avatar></a>
                                         </Link>
                                     )}
--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -5,7 +5,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from '../sagas';
 
-const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
     // console.log("loggerMiddleware",action);          //로그 많음.
     return next(action);
 }
@@ -14,8 +14,11 @@ const configureStore = () => {
     const safaMiddleware = createSagaMiddleware();
     const middlewares = [safaMiddleware, loggerMiddleware];
     const enhancer = process.env.NODE_ENV === 'production'
+
         ? compose(applyMiddleware(...middlewares))
         : composeWithDevTools(applyMiddleware(...middlewares));
+    // ? compose(applyMiddleware(safaMiddleware))
+    // : composeWithDevTools(applyMiddleware(safaMiddleware, loggerMiddleware));
 
     const store = createStore(reducer, enhancer);
     store.sagaTask = safaMiddleware.run(rootSaga);

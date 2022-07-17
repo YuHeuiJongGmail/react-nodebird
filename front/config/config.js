37723f9ffb504601cf1e35d9ed688cb0f8d599d0
YHJ <yhj@YHJui-MacBookPro.local>
2022-07-17 17:41:39 +0900

    front backurl modi

--- front/config/config.js
+++ front/config/config.js
@@ -1,2 +1,2 @@
-export const backUrl = 'https://api.hijong.monster/'; 
+export const backUrl = 'https://api.hijong.monster'; 
 // export const backUrl = '//api.hijong.monster/';  https ==> http로 보내면 mixed contents에러 나므로 http:를 떼면 가능할 수도 있음.
